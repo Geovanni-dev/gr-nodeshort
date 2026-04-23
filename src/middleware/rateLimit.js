ccbfaea927adfbfb74078ffb3478d0b3403c507c
Geovani Rodrigues <geovanni3344@outlook.com>
2026-04-22 21:01:42 -0300

    feat: tratamento de rate limit no front-end e ajuste no botão de submit

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -3,8 +3,21 @@ const rateLimit = require("express-rate-limit"); // Importa o express-rate-limit
 // Configura o rate limit para 5 reqs por minuto
 const limiter = rateLimit({ // Configura o rate limit
     windowMs: 60 * 1000, // 1 minuto
-    max: 10, // Limite de 10 reqs por minuto
-    message: "Limite de Urls encurtadas por minuto atingido, aguarde e tente novamente mais tarde.",
+    max: 15, // Limite de 15 reqs por minuto
+   
+   handler: (req, res) => {
+    res.set("Retry-After", "60");
+
+    res.status(429).json({ // Responde com um status 429 
+        success: false,
+        error: "Muitas tentativas",
+        message: "Muitas Urls encurtadas, tente novamente em:",
+        retryAfter: 60,
+    });
+    }
 });
 
 module.exports = limiter;
+
+
+
